2e399359db189b8a3de1d32a378ca2b566b0cd48
Sihle <msyhlez@gmail.com>
2014-08-25 09:36:24 +0200

    XSL XML Transformation app Modification
    Signed-off-by: Sihle <msyhlez@gmail.com>

---
+++ xProcessor/src/com/sihle/demo/xpoi/ReadExcelDemo.java
@@ -0,0 +1,145 @@
+package com.sihle.demo.xpoi;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.Iterator;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.xml.sax.SAXException;
+
+import com.sun.java_cup.internal.parse_action;
+import com.sun.org.apache.xerces.internal.impl.dv.ValidatedInfo;
+
+public class ReadExcelDemo {
+	static int count;
+	static int rmHeader = 0;
+
+	public static void main(String[] args) {
+		try {
+			FileInputStream file = new FileInputStream(new File(
+			"excelRead_demo.xlsx"));
+
+			// Create Workbook instance holding reference to .xlsx file
+			XSSFWorkbook workbook = new XSSFWorkbook(file);
+
+			// Get first/desired sheet from the workbook
+			XSSFSheet sheet = workbook.getSheetAt(0);
+
+			// Iterate through each rows one by one
+			Iterator<Row> rowIterator = sheet.iterator();
+
+			while (rowIterator.hasNext()) {
+				String S_SERIAL_NO = "";
+				String S_X_ID = "";
+				String SIM = "";
+				count = 0;
+
+				Row row = rowIterator.next();
+				// For each row, iterate through all the columns
+				Iterator<Cell> cellIterator = row.cellIterator();
+				if (rmHeader != 0) {
+					while (cellIterator.hasNext()) {
+						Cell cell = cellIterator.next();
+						// Check the cell type and format accordingly
+						switch (cell.getCellType()) {
+						case Cell.CELL_TYPE_STRING:
+							// System.out.print(cell.getStringCellValue() +
+							// "\t");
+							if (count == 0) {
+								S_SERIAL_NO = cell.getStringCellValue();
+							} else if (count == 1) {
+								S_X_ID = cell.getStringCellValue();
+							} else if (count == 2) {
+								SIM = cell.getStringCellValue();
+							}
+							break;
+						}
+						count++;
+					}
+					// System.out.println(" ===="+S_SERIAL_NO+" "+S_X_ID+" "+SIM);
+
+					// Check if that file is in that directory
+					// If it is then process it.
+					if (validate(System.getProperty("user.dir") + "\\in\\"
+							+ S_SERIAL_NO + ".xml")) {
+						parse(S_X_ID, SIM, S_SERIAL_NO);
+					} else {
+						System.out.println("************* The file is not in that directory *************");
+					}
+				}
+				rmHeader++;
+			}
+			file.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	// Parse each XML file.
+	static void parse(String vBirthDate, String vSIMCardNumber, String xmlName)
+	throws TransformerException, TransformerConfigurationException,
+	FileNotFoundException, IOException {
+		// Use the static TransformerFactory.newInstance() method to instantiate
+		// a TransformerFactory. The javax.xml.transform.TransformerFactory
+		// system property setting determines the actual class to instantiate --
+		// org.apache.xalan.transformer.TransformerImpl.
+		TransformerFactory tFactory = TransformerFactory.newInstance();
+
+		// Use the TransformerFactory to instantiate a Transformer that will
+		// work with
+		// the stylesheet you specify. This method call also processes the
+		// stylesheet
+		// into a compiled Templates object.
+		Transformer transformer = tFactory.newTransformer(new StreamSource(
+		"xslScript.xsl"));
+
+		// Set the parameter. I can't get non-null namespaces to work!!
+		transformer.setParameter("pBirthDate", /* parameter name */
+				vBirthDate /* parameter value */);
+		transformer.setParameter("pSIMCardNumber", /* parameter name */
+				vSIMCardNumber /* parameter value */);
+		// Use the Transformer to apply the associated Templates object to an
+		// XML document
+		// (./in/foo.xml) and write the output to a file (./out/foo.xml).
+		transformer.transform(new StreamSource(System.getProperty("user.dir")
+				+ "\\in\\" + xmlName + ".xml"), new StreamResult(
+						new FileOutputStream(System.getProperty("user.dir") + "\\out\\"
+								+ xmlName + ".xml")));
+
+		System.out.println("************* The result is in "
+				+ System.getProperty("user.dir") + "\\out\\" + xmlName + ".xml"
+				+ " *************");
+	}
+
+	static boolean validate(String fDir) throws FileNotFoundException,
+	IOException, ParserConfigurationException, SAXException {
+		boolean result = false;
+		File dir = new File(fDir);
+
+		if (dir.isFile()) // Just checking one file.
+		{
+			System.out.println("************* Looking for a file at this point. *************");
+			result = true;
+		} else if (dir.isDirectory()) // Checking the contents of a directory.
+		{
+			System.out.println("************* Not looking for a directory at this point. *************");
+		}
+		return result;
+	}
+}
---
+++ xProcessor/xslScript.xsl
@@ -0,0 +1,58 @@
+<?xml version="1.0"?>
+
+  <!--
+   * Licensed to the Apache Software Foundation (ASF) under one
+   * or more contributor license agreements. See the NOTICE file
+   * distributed with this work for additional information
+   * regarding copyright ownership. The ASF licenses this file
+   * to you under the Apache License, Version 2.0 (the  "License");
+   * you may not use this file except in compliance with the License.
+   * You may obtain a copy of the License at
+   *
+   *     http://www.apache.org/licenses/LICENSE-2.0
+   *
+   * Unless required by applicable law or agreed to in writing, software
+   * distributed under the License is distributed on an "AS IS" BASIS,
+   * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   * See the License for the specific language governing permissions and
+   * limitations under the License.
+  -->
+
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
+<xsl:output method="xml" omit-xml-declaration="yes" indent="yes"/>
+<xsl:param name="pBirthDate" select="'default value'"/>
+<xsl:param name="pSIMCardNumber" select="'default value'"/>
+<!-- <xsl:param name="pidNumber" select="'default value'"/> -->
+
+<xsl:strip-space elements="*"/>
+
+<xsl:template match="node()|@*">
+    <xsl:copy>
+        <xsl:apply-templates select="node()|@*"/>
+    </xsl:copy>
+</xsl:template>
+
+<!-- <xsl:template match="Class/Order/Family/Species[@Scientific_Name='Nothocercus']">
+<Species Scientific_Name="Nothocercus">Sihle Mchunu.</Species>
+</xsl:template> -->
+
+<!-- <xsl:template match="Mobile-MSISDN/@BirthDate[.='1969-06-24T00:00:00']"> -->
+<xsl:template match="@BirthDate">
+  <xsl:attribute name="BirthDate">
+    <xsl:value-of select="concat(concat(concat(concat(19,substring($pBirthDate,1,2)),-substring($pBirthDate,3,2)),-substring($pBirthDate,5,2)),'T00:00:00')"/>
+  </xsl:attribute>
+ </xsl:template>
+
+<xsl:template match="@SIMCardNumber">
+  <xsl:attribute name="SIMCardNumber">
+    <xsl:value-of select="$pSIMCardNumber"/>
+  </xsl:attribute>
+ </xsl:template>
+ 
+ <xsl:template match="@idNumber">
+  <xsl:attribute name="idNumber">
+    <xsl:value-of select="$pBirthDate"/>
+  </xsl:attribute>
+ </xsl:template>
+
+</xsl:stylesheet>
